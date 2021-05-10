b157895c2fb4e9b96605f362838f376103e24ce5
Brian Wilson <squarehippobills@gmail.com>
2021-05-10 19:28:34 -0400

    Initial Commit

---
+++ FlagsOfTheWortld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagsOfTheWortld
+//
+//  Created by Brian Wilson on 5/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagsOfTheWortld/FlagsOfTheWortldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsOfTheWortldApp.swift
+//  FlagsOfTheWortld
+//
+//  Created by Brian Wilson on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FlagsOfTheWortldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
